2b403a541d2b764e6c645c5e6c7071104bd027e8
Yuanbin Cai <2394653688@qq.com>
2022-07-29 15:02:51 +0800

    first commit

---
+++ src/Test.java
@@ -0,0 +1,11 @@
+import edu.princeton.cs.algs4.StdIn;
+
+/**
+ * @author Yuanbin Cai
+ * @date 2022/7/24
+ **/
+public class Test {
+    public static void main(String[] args) {
+        StdIn.readAll();
+    }
+}
